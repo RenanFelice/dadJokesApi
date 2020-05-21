e9702077c4697d817c88859e92c4a752ce5c96e1
renanfelice <felice_owns@hotmail.com>
2020-05-21 17:31:01 -0300

    projeto criado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import JokeList from './components/JokeList'
+import Sidebar from './components/Sidebar'
+import './styles/App.css';
+import JokeProvider from './context/JokeContext'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JokeProvider>
+        <Sidebar />
+        <JokeList />
+      </JokeProvider>
     </div>
   );
 }
